3bf37fba549e44b5569eed70db4a5b94b34f046f
Ashvini Dhokane <ashwinidhokane1@gmail.com>
2023-04-08 13:06:31 +0530

    first commit

---
+++ src/main/java/PracticeDemo/Demo2/PracticeDemo2Application.java
@@ -0,0 +1,13 @@
+package PracticeDemo.Demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/PracticeDemo/Demo2/PracticeDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package PracticeDemo.Demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
